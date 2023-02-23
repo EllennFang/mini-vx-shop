ef4eabb118abc7f0bbd0e3f0a4880116539f1e93
zhangsan <zhangsan@qq.com>
2023-02-23 16:55:27 +0800

    新增收货地址

--- power-business/member-service/src/main/java/com/powernode/controller/UserAddrController.java
+++ power-business/member-service/src/main/java/com/powernode/controller/UserAddrController.java
@@ -7,9 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,15 @@ public class UserAddrController {
         String userId = AuthUtil.getLoginUserId();
         List<UserAddr> userAddrList = userAddrService.selectUserAddrList(userId);
         return ResponseEntity.ok(userAddrList);
+    }
+
+//    p/address/addAddr
+    @ApiOperation("新增收货地址")
+    @PostMapping("addAddr")
+    public ResponseEntity<Void> saveUserAddr(@RequestBody UserAddr userAddr) {
+        String userId = AuthUtil.getLoginUserId();
+        userAddr.setUserId(userId);
+        userAddrService.save(userAddr);
+        return ResponseEntity.ok().build();
     }
 }
--- power-business/member-service/src/main/java/com/powernode/service/impl/UserAddrServiceImpl.java
+++ power-business/member-service/src/main/java/com/powernode/service/impl/UserAddrServiceImpl.java
@@ -1,5 +1,6 @@
 package com.powernode.service.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.powernode.domain.UserAddr;
@@ -7,9 +8,11 @@ import com.powernode.mapper.UserAddrMapper;
 import com.powernode.service.UserAddrService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 @Service
 @CacheConfig(cacheNames = "com.powernode.service.impl.UserAddrServiceImpl")
@@ -26,6 +29,28 @@ public class UserAddrServiceImpl extends ServiceImpl<UserAddrMapper, UserAddr> i
         return userAddrMapper.selectList(new LambdaQueryWrapper<UserAddr>()
                 .eq(UserAddr::getUserId,userId)
                 .eq(UserAddr::getStatus,1)
+                .orderByDesc(UserAddr::getCommonAddr,UserAddr::getCreateTime)
         );
+    }
+
+    @Override
+    @CacheEvict(key = "#userAddr.userId")
+    public boolean save(UserAddr userAddr) {
+        //查询用户收货地址列表
+        List<UserAddr> userAddrList = userAddrMapper.selectList(new LambdaQueryWrapper<UserAddr>()
+                .eq(UserAddr::getUserId, userAddr.getUserId())
+                .eq(UserAddr::getStatus, 1)
+        );
+        userAddr.setCommonAddr(0);
+        userAddr.setStatus(1);
+        userAddr.setCreateTime(new Date());
+        userAddr.setUpdateTime(new Date());
+        userAddr.setVersion(0);
+        //判断当前用户是否有收货地址
+        if (CollectionUtil.isEmpty(userAddrList) || userAddrList.size() == 0) {
+            //说明：用户没有收货地址，那当前新增的就为默认收货地址
+            userAddr.setCommonAddr(1);
+        }
+        return userAddrMapper.insert(userAddr)>0;
     }
 }
