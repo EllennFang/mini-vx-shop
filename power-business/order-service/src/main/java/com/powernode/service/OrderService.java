00d890cbee6fb2cd35bd6124e677693e3d9ab1d1
zhangsan <zhangsan@qq.com>
2023-02-23 14:57:04 +0800

    创建订单服务order-service工程

---
+++ power-business/order-service/src/main/java/com/powernode/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.powernode.service;
+
+import com.powernode.domain.Order;
+import com.baomidou.mybatisplus.extension.service.IService;
+public interface OrderService extends IService<Order>{
+
+
+}
