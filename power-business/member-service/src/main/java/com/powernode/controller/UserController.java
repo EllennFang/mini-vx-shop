d4a0d3cd9c9581915dbfac3bf5a987623753d41b
zhangsan <zhangsan@qq.com>
2023-02-23 15:25:16 +0800

    查询用户是否绑定手机号码

---
+++ power-business/member-service/src/main/java/com/powernode/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.powernode.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.powernode.domain.User;
+import com.powernode.service.UserService;
+import com.powernode.utils.AuthUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "用户接口管理")
+@RequestMapping("p/user")
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @ApiOperation("查询用户是否绑定手机号码")
+    @GetMapping("isBindPhone")
+    public ResponseEntity<Boolean> isBindPhone() {
+        String userId = AuthUtil.getLoginUserId();
+        User user = userService.getOne(new LambdaQueryWrapper<User>()
+                .eq(User::getUserId, userId)
+        );
+        return ResponseEntity.ok(StringUtils.hasText(user.getUserMobile()));
+    }
+}
