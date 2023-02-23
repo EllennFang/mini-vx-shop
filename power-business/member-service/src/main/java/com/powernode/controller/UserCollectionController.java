e3acd7b8f4be63b908f36532e549df4440aed0b9
zhangsan <zhangsan@qq.com>
2023-02-23 15:19:24 +0800

    查询用户收藏商品的数量

---
+++ power-business/member-service/src/main/java/com/powernode/controller/UserCollectionController.java
@@ -0,0 +1,32 @@
+package com.powernode.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.powernode.domain.UserCollection;
+import com.powernode.service.UserCollectionService;
+import com.powernode.utils.AuthUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "用户收藏接口管理")
+@RequestMapping("p/collection")
+@RestController
+public class UserCollectionController {
+
+    @Autowired
+    private UserCollectionService userCollectionService;
+
+    @ApiOperation("查询用户收藏商品的数量")
+    @GetMapping("count")
+    public ResponseEntity<Integer> loadUserCollectionCount() {
+        String userId = AuthUtil.getLoginUserId();
+        int count = userCollectionService.count(new LambdaQueryWrapper<UserCollection>()
+                .eq(UserCollection::getUserId, userId)
+        );
+        return ResponseEntity.ok(count);
+    }
+}
